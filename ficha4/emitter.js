0be3e267968babe00a92f8bbcb182d1f63c8e257
roberto silva <bertocarlos.rs@gmail.com>
2023-03-28 18:46:05 +0100

    Create emitter.js

---
+++ ficha4/emitter.js
@@ -0,0 +1,22 @@
+class Emitter {
+    constructor(){
+        this.events={}
+    }
+}
+Emitter.prototype.on=function(type,listener){
+    if (this.events[type] == undefined){
+        this.events[type]=[]
+    }
+    this.events[type].push(listener);
+
+}
+
+Emitter.prototype.emit=function(type){
+    if(this.events[type]!= undefined){
+        this.events[type].forEach(listener=>{
+            listener();
+        })
+    }
+}
+
+module.exports=Emitter;
