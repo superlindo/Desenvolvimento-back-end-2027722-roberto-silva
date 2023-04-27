fddef63280a06bf4b2de92b1fc41cbb46178820f
superlindo <bertocarlos.rs@gmail.com>
2023-04-27 10:24:49 +0100

    a

---
+++ ficha7/app.js
@@ -0,0 +1,9 @@
+//usar o node.js command prompt
+//npm install express-generator -global
+//npm init no terminal se for a primeira vez 
+//npm install express se for a primeira vez
+//cd localizaçao da pasta
+//express --view=pug
+//npm install
+//npm start (para fazer o servidor correr)
+//npm install mysql
