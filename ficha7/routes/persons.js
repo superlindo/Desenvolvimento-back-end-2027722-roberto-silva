c31186fc8c82465da299d11256e06988f57f8002
roberto silva <bertocarlos.rs@gmail.com>
2023-05-02 19:32:28 +0100

    a

--- ficha7/routes/persons.js
+++ ficha7/routes/persons.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var connection=mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'password',
+    password:'',
     database:'ficha7'
 });
 
@@ -18,5 +18,39 @@ router.get('/', function(req, res, next) {
       res.send(results);
   })
 });
+
+
+/* GET users ID listing. */
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id;
+  connection.query('SELECT * FROM persons WHERE idpersons = ?', id,(err,results,fields)=>{   
+      res.send(results);
+  })
+});
+
+/* DELETE users */
+router.delete('/:id', function(req, res, next) {
+  var id = req.params.id;
+  connection.query('DELETE FROM persons WHERE idpersons = ?', id,(err,results,fields)=>{   
+      res.send(results);
+  })
+});
+
+/* GET users AGE & LASTNAME listing. */
+router.get('/:age/:lastname', function(req, res, next) {
+  connection.query('SELECT * FROM persons WHERE age = ? and lastname = ?', [req.params.age,req.params.lastname],(err,results,fields)=>{   
+      res.send(results);
+  })
+});
+
+
+/* POST users (Add details of new person) */
+router.get('/', function(req, res, next) {
+  var person=req.body;
+  connection.query("INSERT INTO persons SET = ", [person],(err,results,fields)=>{
+      res.send(results);
+  })
+});
+
 
 module.exports = router;
